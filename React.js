f68a40d1a52bb1efc51755a9176022013c52894f
BISOGNO Gabriele <gabriele.bisogno@soprasteria.com>
2019-03-13 16:54:27 +0100

    updates

---
+++ React.js
@@ -0,0 +1,14 @@
+ReactDOM.render(JSX, document.getElementById('root')
+
+//create a React Component with ES6 class sytax
+class Kitten extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hi</h1>
+    );
+  }
+}
